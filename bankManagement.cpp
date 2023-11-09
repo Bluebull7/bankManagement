e405cde545943f7f0a9b4b8267e58876a593779d
Lance <lsaucyfx@gmail.com>
2023-11-09 16:53:18 -0500

    Modified bankMgmt file

--- bankManagement.cpp
+++ bankManagement.cpp
@@ -20,5 +20,31 @@ public:
     void modify(); //function to add new data
     void dep(int); //function to accept amount and add to balance 
     void draw(int); // function to accept amount and sub from balance
+    void report() const; //function to show data in tabular format
+    int retacno() const; // function to return account number
+    int retdeposit() const; //function to return balance amount
+    char rettype() const; //function to return type of account
 
 };
+
+void account::create_account()
+{
+    cout<<"\nEnter The account No. :";
+    cin>>acno;
+    cout<<"\n\nEnter The Name of The account Holder : ";
+    cin.ignore()   
+    cin.getline(name,50);
+    cout<<"\nEnter the Type of The account (C/S) :";
+    cin>>type;
+    type=toupper(type);
+    cout<<"\nEnter The Initial amount (>=500 for Saving and >=1000 for current ) : " ;
+    cin>>deposit;
+    cout<<"\n\n\nAccount Created..";
+}
+void account::show_account() const
+
+{
+    cout<<"\nAccount No. : "<<acno;
+}
+    
+

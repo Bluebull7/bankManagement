86769c75f808d9e4f4f0e3f2ad1fd93f9524e308
Lance <lsaucyfx@gmail.com>
2023-11-10 16:53:29 -0500

    updated main files

--- bankManagement.cpp
+++ bankManagement.cpp
@@ -42,9 +42,148 @@ void account::create_account()
     cout<<"\n\n\nAccount Created..";
 }
 void account::show_account() const
-
 {
-    cout<<"\nAccount No. : "<<acno;
+    cout<<"\nAccount No. : "<<acno;;
+    cout<<"\nAccount Holder Name : ";
+    cout<<name;
+    cout<<"\nType of Account : "<<type;
+    cout<<"\nBalance amount : "<<deposit;
 }
     
+void account::modify()
+{
+    cout<<"\nAccount No, : "<<acno;
+    cout<<"\nModify Account Holder Name : ";
+    cin.ignore();
+    cin.getline(name,50)
+    cout<<"\nModify Type of Account" <<type;
+    cout<"\nBalance amount : "<<deposit;
+    cin>>type;
+    type = toupper(type)
+    cout<<"\nModify Balance amount : ";
+    cin>>deposit;
+}
 
+void account::dep(int x)
+{
+    deposit+=x;
+
+}
+void account::draw(int x)
+{
+    deposit-=x;
+
+}
+void account::report() const
+{
+    cout<<acno<<setw()<<" "<<name<<setw(10)
+<<" "<<type<<setw(6)<<deposit<<endl;
+}
+
+int account::retdeposit() const
+{
+    return deposit;
+}
+
+char account::rettype() const
+{
+    return type;
+}
+
+// function declaration
+
+void write_account(); //function to write record in binary file
+void display_sp(int); // function to display account details given by user
+void modify_account(int); // function to modify acc details, given by user
+void delete_account(int) //function to delete record of file
+void display_all(); //function to display all account details
+void deposit_withdraw(int, int); // function to depoit/withdraw amount for given account
+void intro(); // introductory screen function
+
+
+// main function
+
+int main()
+
+{
+    
+    char ch;
+    int num;
+    intro();
+    do
+    {
+
+        system("cls");
+        cout<<"\n\n\n\tMAIN MENU";
+        cout<<"\n\n\t01. NEW ACCOUNT";
+        cout<<"\n\n\t02. DEPOSIT AMOUNT";
+        cout<<"\n\n\t03. WITHDRAW AMOUNT";
+        cout<<"\n\n\t04. BALANCE ENQUIRY";
+        cout<<"\n\n\t05. ALL ACCOUNT HOLDER LIST";
+        cout<<"\n\n\t06. CLOSE AN ACCOUNT";
+        cout<<"\n\n\t07. MODIFY AN ACCOUNT";
+        cout<<"\n\n\t08. EXIT";
+        cout<<"\n\n\tSelect Your Option (1-8)";
+        cin>>ch;
+        system("cls");
+        switch(ch)
+        {
+        case '1':
+            write_account()
+            break;
+        case '2':
+            cout<<"\n\n\tEnter the account No.";
+        cin>>num;
+        deposit_withdraw(num,1);
+        break;
+
+        case '3':
+            cout<<"\n\n\tEnter The account No. : ";
+            cin>>num;
+            deposit_withdraw(num,2);
+            break;
+
+        case '4':
+            cout<<"\n\n\tEnter The account No. : ";
+            cin>>num;
+            display_sp(num);
+            break;
+
+        case '5':
+            display_all();
+            break;
+        case '6':
+            cout<<"\n\n\tEnter the account No. : ";
+            cin>>num;
+            delete_account(num);
+            break;
+
+        case '7':
+            cout<<"\n\n\tEnter The account No. : ";
+            cin>>num;
+            modify_account(num);
+            break;
+        
+        case '8':
+            cout<<"\n\n\tThanks for using bank management system";
+            break;
+        default :cout"\a";
+        }
+        cin.ignore();
+        cin.get();
+
+    }while(ch!='8');
+    return 0;
+}
+
+// function to write in file
+
+void write_account()
+{
+
+    account ac;
+    ofstream outFile;
+    outFile.open("account.dat", ios::binary | ios:: app);
+    ac.create_account();
+    outFile.write
+}
